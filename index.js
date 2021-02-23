66f6ab81e869b109c52273398de8bd83c6cdd28e
Shevkenov Andrey <shevkenov@gmail.com>
2021-02-23 20:28:16 +0200

    add middleware to handle both routes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import routes from './routes/routes.js';
+import postRouter from './routes/postRoutes.js';
+import userRouter from './routes/userRouters.js';
 
 const app = express();
 dotenv.config();
@@ -17,7 +18,8 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
-app.use('/posts', routes);
+app.use('/posts', postRouter);
+app.use('/user', userRouter);
 
 app.use(express.static('./client/build'));
 
