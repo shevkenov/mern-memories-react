1d61c7ac863ceacf9ad3107a26558e397b7c019e
Shevkenov Andrey <shevkenov@gmail.com>
2021-02-24 19:47:28 +0200

    fix some problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container } from '@material-ui/core';
-import { Route, Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
