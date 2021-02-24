e7bb1ea6992ab75f9339127c3d5a6b601fd57a1e
Shevkenov Andrey <shevkenov@gmail.com>
2021-02-24 19:50:17 +0200

    change the url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 //const url = 'https://mern-mems.herokuapp.com/posts';
-const url = 'http://localhost:5000/posts'
+const url = 'http://localhost:5000'
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (data) => axios.post(url, data);
-export const removePost = (id) => axios.delete(`${url}/${id}`);
-export const likedPost = (id) => axios.patch(`${url}/${id}/like`);
-export const updatePost = (id, data) => axios.patch(`${url}/${id}`, data);
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const createPost = (data) => axios.post(`${url}/posts`, data);
+export const removePost = (id) => axios.delete(`${url}/posts/${id}`);
+export const likedPost = (id) => axios.patch(`${url}/posts/${id}/like`);
+export const updatePost = (id, data) => axios.patch(`${url}/posts/${id}`, data);
+export const login = (data) => axios.post(`${url}/user/login`, data);
+export const signUp = (data) => axios.post(`${url}/user/signup`, data);
