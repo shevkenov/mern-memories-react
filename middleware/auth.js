b8fa8a142d6235ba62bfd9d043f859d75aeea115
Shevkenov Andrey <shevkenov@gmail.com>
2021-02-24 19:15:09 +0200

    add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { CLIENT_RENEG_WINDOW } from 'tls';
+
+const secret = 'something very secret';
+
+export default async(req, res, next) => {
+    try {
+        console.log(req.headers);
+        
+        const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+        const isCustomAuth = token.leght < 500;
+
+        let decodedData;
+        if(token && isCustomAuth){
+            decodedData = jwt.verify(token, secret);
+        }else{
+            decodedData = jwt.decode(token);
+        }
+
+        req.userId = decodedData?.id;
+
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
