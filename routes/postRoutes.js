12dc20bb06054c9b8b8f9fff66c9b218d755f601
Shevkenov Andrey <shevkenov@gmail.com>
2021-02-24 19:16:32 +0200

    protect routes with auth middleware

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
 import { getPost, getPosts, deletePost, likedPost, updatePost, createPost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const postRouter = express.Router();
 
-postRouter.post('/:id', getPost);
-postRouter.delete('/:id', deletePost);
-postRouter.patch('/:id', updatePost);
-postRouter.patch('/:id/like', likedPost);
-postRouter.post('/', createPost);
+//postRouter.post('/:id', getPost);
+postRouter.delete('/:id', auth, deletePost);
+postRouter.patch('/:id', auth, updatePost);
+postRouter.patch('/:id/like', auth, likedPost);
+postRouter.post('/', auth, createPost);
 postRouter.get('/', getPosts);
 
 export default postRouter;
