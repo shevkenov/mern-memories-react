b38d90d7bfed6af96076c6cfb4cc624e1f4c5658
Shevkenov Andrey <shevkenov@gmail.com>
2021-03-01 18:05:13 +0200

    add auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,9 +4,15 @@ const initialState = { isLoggedIn: false, loading: false, authData: null, error:
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case actionNames.LOGIN_START:
+            return {...state, loading: true}
+
+        case actionNames.LOGIN_ERROR:
+            return {...state, loading: false, error: action.payload}
+
         case actionNames.LOGIN:
             localStorage.setItem('token', action.payload.token);
-            return { ...state, isLoggedIn: true, authData: action.payload, loading: false}
+            return { ...state, isLoggedIn: true, authData: action.payload, loading: false, error: null}
 
         case actionNames.LOGOUT:
             localStorage.clear();
